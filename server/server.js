7f5650e9ae6ea6d0045fd015b3fac86f230444c9
Victor <oancea.mircea@gmail.com>
2019-02-17 21:05:02 +0200

    test DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.delete('/todos/:id', (req, res) => {
     if (!todo) {
       res.status(404).send();
     }
-    res.status(200).send(todo);
+    res.status(200).send({todo});
     })
     .catch((e) => res.status(404).send())
 });
